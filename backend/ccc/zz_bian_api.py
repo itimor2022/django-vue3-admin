c46f8cdd7ed87071aba4f22658d6721f13e6d900
itimor <itimor@126.com>
2024-10-30 00:59:50 +0700

    add

--- backend/ccc/zz_bian_api.py
+++ backend/ccc/zz_bian_api.py
@@ -6,12 +6,13 @@ import time
 period = '5m'
 typ = 'USDT'
 
+t = int(time.time())
+print(t)
+
 
 def get_pairs():
     exclude_pair_list = ['USDCUSDT', 'BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'PEPEUSDT', 'DOGEUSDT', 'TONUSDT', 'XRPUSDT',
                          'BCHUSDT', 'LTCUSDT', 'SHIBUSDT', 'TONUSDT']
-    t = int(time.time())
-    print(t)
     url = f"https://www.okx.com/priapi/v5/rubik/web/public/up-down-rank?countryFilter=1&rank=0&zone=utc8&period={period}&type={typ}&t={t}"
     r = requests.get(url)
     c = r.json()['data']['data']
@@ -26,7 +27,7 @@ def get_pairs():
 
 def main():
     pair_list = ['ICPUSDT']
-    #pair_list = get_pairs()
+    # pair_list = get_pairs()
     type_b = '做多'
     type_s = '做空'
 
@@ -46,21 +47,21 @@ def main():
         k['s'] = 0.0
         if len(d1) > 0:
 
-            # if float(d1[0]['longShortRatio']) > 1:
-            #     t1 = float(d1[0]['longAccount']) / float(d1[1]['shortAccount'])
-            #     t2 = float(d1[0]['longAccount']) / float(d1[2]['longAccount'])
-            #     if t1 > m or t2 > m:
-            #         print(t1)
-            #         print(t2)
-            #         print(f'多倍{type_b}')
-            # else:
-            #     t1 = float(d1[0]['shortAccount']) / float(d1[1]['longAccount'])
-            #     t2 = float(d1[0]['shortAccount']) / float(d1[2]['longAccount'])
-            #     if t1 > m or t2 > m:
-            #         print(t1)
-            #         print(t2)
-            #         print(f'多倍{type_s}')
-            # k['t'] = max(t1, t2)
+            if float(d1[0]['longShortRatio']) > 1:
+                t1 = float(d1[0]['longAccount']) / float(d1[1]['shortAccount'])
+                t2 = float(d1[0]['longAccount']) / float(d1[2]['longAccount'])
+                if t1 > m or t2 > m:
+                    print(t1)
+                    print(t2)
+                    print(f'多倍{type_b}')
+            else:
+                t1 = float(d1[0]['shortAccount']) / float(d1[1]['longAccount'])
+                t2 = float(d1[0]['shortAccount']) / float(d1[2]['longAccount'])
+                if t1 > m or t2 > m:
+                    print(t1)
+                    print(t2)
+                    print(f'多倍{type_s}')
+            k['t'] = max(t1, t2)
 
             r2 = futures_client.taker_long_short_ratio(pair, period, limit=50)
             r3 = list()
@@ -69,6 +70,7 @@ def main():
                 r['buyVol'] = float(r['buyVol'])
                 r3.append(r)
             d2 = sorted(r3, key=itemgetter('timestamp'), reverse=True)
+            print(d2)
             if float(d2[0]['buySellRatio']) > 1:
                 d1 = sorted(r3, key=itemgetter('buyVol'), reverse=True)
                 s1 = d2[0]['buyVol'] / d2[1]['buyVol']
@@ -79,14 +81,13 @@ def main():
                     print(f'多倍{type_b}')
             else:
                 d1 = sorted(r3, key=itemgetter('sellVol'), reverse=True)
-                s1 = d1[0]['sellVol'] / d1[1]['sellVol']
-                s2 = d1[0]['sellVol'] / d1[2]['sellVol']
+                s1 = d2[0]['sellVol'] / d2[1]['sellVol']
+                s2 = d2[0]['sellVol'] / d2[2]['sellVol']
                 if s1 > n or s2 > n and d1[0] is d2[0]:
                     print(s1)
                     print(s2)
                     print(f'多倍{type_s}')
             print(d1)
-            print(d2)
             k['s'] = max(s1, s2)
 
         h.append(dict(k))
